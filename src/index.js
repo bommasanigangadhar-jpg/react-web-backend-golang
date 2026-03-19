ca957ae78fa29a3eec597348fd894b1a82553313
Gangadhar <bommasani.gangadhar@gmail.com>
2026-03-20 01:48:20 +0530

    [no task id]-added router related changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import Movies from './components/Movies';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <Home /> },
+      {
+        path: "/movies",
+        element: <Movies />,
+      }
+    ]
+  }
+
+])
 root.render(
   <React.StrictMode>
-    <div className='container'>
-      <div className='row'>
-        <div className='col'>
-            <App message="State using classes"/>
-        </div>
-      </div>
-    </div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
